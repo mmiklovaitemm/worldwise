2252e9535b49e02f228fb50accac98147b7036ab
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-01-06 17:20:33 +0200

    Updates

--- assets/PageNotFound-bf7e379a.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-b42d6781.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
---
+++ assets/PageNotFound-c6c7e295.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-14c07097.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
